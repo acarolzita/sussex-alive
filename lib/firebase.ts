d0504595dcc24de0d9947bfcdbe6be0be513f2de
Anna Cosenza <acarolzita@gmail.com>
2025-04-27 01:55:46 +0100

    !

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,17 @@
 // lib/firebase.ts
 import { initializeApp } from "firebase/app";
-import { getAuth }     from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
-const clientConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "your-firebase-project-id",
-  // etc...
+const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
+  projectId: "YOUR_FIREBASE_PROJECT_ID",
+  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_FIREBASE_SENDER_ID",
+  appId: "YOUR_FIREBASE_APP_ID"
 };
 
-const app  = initializeApp(clientConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
