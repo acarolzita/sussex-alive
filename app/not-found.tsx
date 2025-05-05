bf490488e4782d0ffa1ae1c553d8d1b1e8b73dee
Anna Cosenza <acarolzita@gmail.com>
2025-05-05 17:17:54 +0100

    !

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFoundPage() {
+    return (
+      <div className="main-container">
+        <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+        <p className="mb-4">Oops! Looks like this page doesn’t exist.</p>
+        <a href="/" className="btn btn-primary">
+          Back to Home
+        </a>
+      </div>
+    );
+  }
+  
